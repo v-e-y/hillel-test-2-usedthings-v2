00caf076f9b77a7ba968a3839864f27482cfe6b2
V-E-Y <v.eduard.y@gmail.com>
2022-07-29 17:19:19 +0300

    ref text

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,7 +44,7 @@ class AuthController extends Controller
         }
         
         return back()->withErrors([
-            'email' => 'test The provided credentials do not match our records.',
+            'email' => 'The provided credentials do not match our records.',
         ])->onlyInput('email');
     }
 }
